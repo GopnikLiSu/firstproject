71c91b00cd32c2b7e5cbf6bbcd99e6a8f594d891
Alex Bash <dr.bazhenoff2017@yandex.ru>
2018-05-25 20:33:28 +0600

    revu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # REVU: Why do u need @user? It is not used in view
     @user = current_user
   #  @last_10_rate = Usd.last(10).map{|x| [x.date.to_s.gsub('-', '.').split('.').reverse.join('.'), x.rate.to_f]}
   #  @last_10_rate_min = @last_10_rate.min{|a, b| a[1] <=> b[1]}[1] - 0.5
@@ -10,6 +11,7 @@ class HomeController < ApplicationController
   end
 
   def my_currency
+    # REVU: Why do u need @user? It is not used in view
     @user = current_user
     #@user.rates.delete(@user.rates.last)
     #@user.rates << Rate.last(5)
--- app/jobs/update_rate.rb
+++ app/jobs/update_rate.rb
@@ -4,41 +4,45 @@ require 'json'
 class UpdateRate < ApplicationJob
   def perform
       if (Time.now - Usd.order(:created_at).last.created_at) > 1.day
+        # REVU: Why use begin without rescue?
         begin
           url = URI.parse('http://www.cbr-xml-daily.ru/daily_json.js')
           req = Net::HTTP::Get.new(url.to_s)
+          # REVU: Use Unirest instead of Net::HTTP
           res = Net::HTTP.start(url.host, url.port) do |http|
             http.request(req)
           end
-        hash = JSON.parse(res.body)
+          hash = JSON.parse(res.body)
 
-        rate_u = hash['Valute']['USD']['Value']
-        usd_rur = Rate.new
-        usd_rur.rate = rate_u
-        usd_rur.curr_1 = 'USD'
-        usd_rur.curr_2 = 'RUR'
-        usd_rur.save!
 
-        rate_e = hash['Valute']['EUR']['Value']
-        eur_rur = Rate.new
-        eur_rur.rate = rate_e
-        eur_rur.curr_1 = 'EUR'
-        eur_rur.curr_2 = 'RUR'
-        eur_rur.save!
 
-        rate_c = hash['Valute']['CNY']['Value']
-        eur_rur = Rate.new
-        eur_rur.rate = rate_c
-        eur_rur.curr_1 = 'CNY'
-        eur_rur.curr_2 = 'RUR'
-        eur_rur.save!
+          rate_u = hash['Valute']['USD']['Value']
+          usd_rur = Rate.new
+          usd_rur.rate = rate_u
+          usd_rur.curr_1 = 'USD'
+          usd_rur.curr_2 = 'RUR'
+          usd_rur.save!
 
-        rate_j = hash['Valute']['JPY']['Value']
-        eur_rur = Rate.new
-        eur_rur.rate = rate_j
-        eur_rur.curr_1 = 'JPY'
-        eur_rur.curr_2 = 'RUR'
-        eur_rur.save!
+          rate_e = hash['Valute']['EUR']['Value']
+          eur_rur = Rate.new
+          eur_rur.rate = rate_e
+          eur_rur.curr_1 = 'EUR'
+          eur_rur.curr_2 = 'RUR'
+          eur_rur.save!
+
+          rate_c = hash['Valute']['CNY']['Value']
+          eur_rur = Rate.new
+          eur_rur.rate = rate_c
+          eur_rur.curr_1 = 'CNY'
+          eur_rur.curr_2 = 'RUR'
+          eur_rur.save!
+
+          rate_j = hash['Valute']['JPY']['Value']
+          eur_rur = Rate.new
+          eur_rur.rate = rate_j
+          eur_rur.curr_1 = 'JPY'
+          eur_rur.curr_2 = 'RUR'
+          eur_rur.save!
         end
     end
   end
